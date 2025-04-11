7c8841fd4707cd8b8e6083c59e7956177ca39eab
miguelant <miguelmantonio@outlook.pt>
2025-04-11 07:46:00 +0100

    Manutenção

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -5,10 +5,11 @@ import RegisterPage from './pages/RegisterPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
 import InitialPage from './pages/InitialPage';
 import InsertRepositoryPage from './pages/InsertRepositoryPage';
+import ComparePredictions from './pages/ComparePredictions'; 
 import GradePredictions from './components/GradePredictions';
-import ComparePredictions from './pages/ComparePredictions';
-import CompareGroups from './pages/CompareGroups'; 
 import LayoutWithSideBar from './components/LayoutWithSideBar';
+import ResetPasswordConfirmPage from './pages/ResetPasswordConfirmPage'; 
+import CompareGroups from './pages/CompareGroups'; 
 
 function App() {
   return (
@@ -18,25 +19,40 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<RegisterPage />} />
         <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password/:uid/:token" element={<ResetPasswordConfirmPage />} />
 
-        <Route path="/initialpage" element={
-          <LayoutWithSideBar>
-            <InitialPage />
-          </LayoutWithSideBar>
-        } />
-
-        <Route path="/insertrepository" element={
-          <LayoutWithSideBar>
-            <InsertRepositoryPage />
-          </LayoutWithSideBar>
-        } />
-
-        <Route path="/gradepredictions" element={
-          <LayoutWithSideBar>
-            <GradePredictions />
-          </LayoutWithSideBar>
-        } />
-
+        <Route
+          path="/initialpage"
+          element={
+            <LayoutWithSideBar>
+              <InitialPage />
+            </LayoutWithSideBar>
+          }
+        />
+        <Route
+          path="/insertrepository"
+          element={
+            <LayoutWithSideBar>
+              <InsertRepositoryPage />
+            </LayoutWithSideBar>
+          }
+        />
+        <Route
+          path="/gradepredictions"
+          element={
+            <LayoutWithSideBar>
+              <GradePredictions />
+            </LayoutWithSideBar>
+          }
+        />
+        <Route
+          path="/compare-predictions"
+          element={
+            <LayoutWithSideBar>
+              <ComparePredictions />
+            </LayoutWithSideBar>
+          }
+        />
         <Route path="/comparepredictions" element={
           <LayoutWithSideBar>
             <ComparePredictions />
